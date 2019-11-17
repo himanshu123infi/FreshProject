dd86893ab01ce789b0861455deb21e1e8775f4bf
himanshu123infi <57826974+himanshu123infi@users.noreply.github.com>
2019-11-17 22:26:28 +0530

    Update Library.java
    commented library.java

--- Library/src/Library.java
+++ Library/src/Library.java
@@ -21,7 +21,7 @@ public class Library extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	//public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
